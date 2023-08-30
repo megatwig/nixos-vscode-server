8e1a86e4ef3bc62fe56e76c2c4a28bad42c34f52
megatwig <137281832+megatwig@users.noreply.github.com>
2023-08-30 18:38:58 +0100

    Fix `mkDefault` usage
    Building this module gives an error as `mkDefault` is being referenced wrong.
    
    ```
    error: undefined variable 'mkDefault'
    
           at /nix/store/7iwgahafx2d0drhvxim4j1wzvv1dn11d-source/modules/_module.nix:4:54:
    
                3| in {
                4|   vscode-cli = mkVSCodeService "cli" { installPath = mkDefault "~/.vscode-cli/code-stable"; };
                 |                                                      ^
                5|   vscode-server = mkVSCodeService "server" { installPath = mkDefault "~/.vscode-server"; };
    ```

--- modules/_module.nix
+++ modules/_module.nix
@@ -1,6 +1,6 @@
 moduleConfig: { lib, ... }: let
   mkVSCodeService = import ../lib/mkVSCodeService.nix;
 in {
-  vscode-cli = mkVSCodeService "cli" { installPath = mkDefault "~/.vscode-cli/code-stable"; };
-  vscode-server = mkVSCodeService "server" { installPath = mkDefault "~/.vscode-server"; };
+  vscode-cli = mkVSCodeService "cli" { installPath = lib.mkDefault "~/.vscode-cli/code-stable"; };
+  vscode-server = mkVSCodeService "server" { installPath = lib.mkDefault "~/.vscode-server"; };
 }
